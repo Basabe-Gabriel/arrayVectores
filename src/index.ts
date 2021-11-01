9f08cd47e139072443833314f535f21eacf9a41d
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-01 20:51:31 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let numero: number[] = new Array(6);
+let numeroIgresado: number;
+
+for (let i = 0; i <= 8; i++) {
+  //numeroIgresado = Number(prompt("ingrese un Numero"));
+  numero[i] = numeroIgresado;
+}
+for (let i = 0; i <= 8; i++) {
+  console.log(numero[i]);
+}
